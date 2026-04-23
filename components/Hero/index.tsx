4a2bd90731747449efe1f6c7b2d4526b0b554f26
U-Kings <kingsleyigbokwe909@gmail.com>
2026-04-23 05:05:49 +0100

    hero section update

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,12 +7,13 @@ const Hero = (props: Props) => {
   return (
     <>
       <section className="relative overflow-hidden bg-white md:pt-32.5 lg:pt-32">
-        <div className="absolute md:mt-32.5 lg:mt-32 bg-linear-to-b from-black/90 to-transparent top-0 bottom-0 left-0 right-0 z-10">
-          <div className="text-white h-40">
-            <h1></h1>
+        <div className="absolute md:mt-32.5 lg:mt-32 bg-linear-to-b from-black/90 to-black/70 top-0 bottom-0 left-0 right-0 z-10">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white h-40 text-center flex flex-col items-center">
+            <h1 className="text-6xl font-bold">Let&apos;s Deal Values</h1>
+            <p className="text-md font-light mt-4 w-4/6">Combines web development, cloud computing, IT services, and professional training to provide comprehensive digital solutions for businesses.</p>
           </div>
         </div>
-        <div className="overflow-hidden h-[60rem]">
+        <div className="overflow-hidden h-170">
           <Image
             src="/images/hero-img.jpg"
             width={1080}
