dd650b8b33a058367e15c83a2c191e073e1c2c89
U-Kings <kingsleyigbokwe909@gmail.com>
2026-07-25 23:46:17 +0100

    know more button remove from about page

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -125,13 +125,16 @@ const About = () => {
                     </span>
                   </p>
                 </div>
-
-                <Link
-                  href="/about"
-                  className="inline-flex items-center justify-center rounded-md bg-primary px-7 py-3 text-center text-base font-medium text-white duration-300 hover:bg-primary/90"
-                >
-                  Know More
-                </Link>
+                {pathUrl !== "/about" ? (
+                  <Link
+                    href="/about"
+                    className="inline-flex items-center justify-center rounded-md bg-primary px-7 py-3 text-center text-base font-medium text-white duration-300 hover:bg-primary/90"
+                  >
+                    Know More
+                  </Link>
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
 
